ac372ce79d821a8ff95ad89a9891632a9ad19647
Crul <rauljavier.vila@gmail.com>
2016-07-25 22:36:09 +0200

    meteor package configuration

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'crul:meteor-angular-external-db-manager',
-  version: '0.0.4',
+  version: '0.0.6',
   summary: 'meteor angular db manager component',
   git: 'https://github.com/Crul/meteor-angular-external-db-manager',
   documentation: 'README.md'
@@ -12,15 +12,14 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-
-  api.versionsFrom('1.3.2.4');
+  api.versionsFrom('1.3.5.1');
   api.use('ecmascript', 'server');
   api.use('underscore');
   api.use('pbastowski:angular-babel@1.3.6', 'client');
   api.use('angular-templates@1.0.3', 'client');
   api.use('driftyco:ionic@1.2.4', 'client');
   api.use('crul:meteor-external-mongo-db@0.3.3');
-  /*
+  
   api.addFiles([
     'client/lib/angular.module.js',
     'client/components/onOffButton/on-off-button.view.ng.html',
@@ -30,18 +29,19 @@ Package.onUse(function(api) {
     'client/components/externalDbManager/external-db-manager.directive.ng.js',
     //'client/components/externalDbmanager/external-db-manager.scss'
   ], 'client');
-  */
+  
   api.mainModule('meteor-angular-external-db-manager.js');
 });
 
 Package.onTest(function(api) {
+  api.versionsFrom('1.3.5.1');
   api.use('ecmascript', 'server');
   api.use('underscore');
   api.use('tinytest');
   api.use('pbastowski:angular-babel@1.3.6', 'client');
   api.use('angular-templates@1.0.3', 'client');
   api.use('driftyco:ionic@1.2.4', 'client');
-  api.use('crul:meteor-external-mongo-db');
+  api.use('crul:meteor-external-mongo-db@0.3.3');
   api.use('crul:meteor-angular-external-db-manager');
   api.mainModule('meteor-angular-external-db-manager-tests.js');
 });
