7daec44f32dd8ce180aaf5e8001e6210397cb318
Crul <rauljavier.vila@gmail.com>
2016-07-26 20:42:49 +0200

    remove html assets

--- package.js
+++ package.js
@@ -1,26 +1,17 @@
 Package.describe({
   name: 'crul:meteor-angular-external-db-manager',
-  version: '0.0.18',
+  version: '0.0.19',
   summary: '[not working] meteor angular db manager component',
   git: 'https://github.com/Crul/meteor-angular-external-db-manager',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  /*
-  Npm.depends({
-    'angular': "1.5.5",
-    'angular-meteor': "1.3.10"
-  });
-  */
   api.versionsFrom('1.3.5.1');
   api.use('ecmascript', 'server');
   api.use('underscore');
   api.use('templating');
-      //api.use('pbastowski:angular-babel@1.3.6', 'client');
-      //api.use('angular-templates', 'client');
   api.use('angular:angular@1.5.3_1', 'client');
-  //api.imply('angular:angular@1.5.3_1', 'client');
   api.use('driftyco:ionic@1.2.4', 'client');
   api.use('crul:meteor-external-mongo-db@0.3.4');
   
@@ -29,12 +20,6 @@ Package.onUse(function(api) {
     'client/components/onOffButton/on-off-button.directive.ng.js',
     'client/components/externalDbManager/external-db-subscriber.service.ng.js',
     'client/components/externalDbManager/external-db-manager.directive.ng.js'
-  ], 'client');
-
-  api.addAssets([
-    'client/components/onOffButton/on-off-button.view.ng.html',
-    'client/components/externalDbManager/external-db-manager.view.ng.html',
-    //'client/components/externalDbmanager/external-db-manager.scss'
   ], 'client');
   
   api.mainModule('meteor-angular-external-db-manager.js', 'server');
